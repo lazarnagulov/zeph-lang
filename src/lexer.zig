1c73085241f37faf45aeef551bb0c835908b9837
Lazar Nagulov <lazarnagulov1@gmail.com>
2024-07-06 18:10:35 +0200

    feat: Add lexing one char

---
+++ src/lexer.zig
@@ -0,0 +1,51 @@
+const t = @import("token.zig");
+
+const Token = t.Token;
+const TokenType = t.TokenType;
+
+pub const Lexer = struct {
+    input: []const u8,
+    position: usize,
+    readPosition: usize,
+    char: u8,
+
+    const Self = @This();
+
+    pub fn init(input: []const u8) Lexer {
+        var lexer = Lexer{
+            .input = input,
+            .position = 0,
+            .readPosition = 0,
+            .char = '0',
+        };
+        lexer.readChar();
+        return lexer;
+    }
+
+    pub fn GetNextToken(self: *Self) Token {
+        const token = switch (self.char) {
+            '=' => Token.init(.assign, &[_]u8{self.char}),
+            ';' => Token.init(.semicolon, &[_]u8{self.char}),
+            ':' => Token.init(.colon, &[_]u8{self.char}),
+            '(' => Token.init(.left_paren, &[_]u8{self.char}),
+            ')' => Token.init(.right_paren, &[_]u8{self.char}),
+            '{' => Token.init(.left_brace, &[_]u8{self.char}),
+            '}' => Token.init(.right_brace, &[_]u8{self.char}),
+            '0' => Token.init(.eof, &[_]u8{self.char}),
+            else => Token.init(.illegal, &[_]u8{self.char}),
+        };
+
+        self.readChar();
+        return token;
+    }
+
+    fn readChar(self: *Self) void {
+        if (self.readPosition > self.input.len) {
+            self.char = '0';
+        } else {
+            self.char = self.input[self.readPosition];
+        }
+        self.position = self.readPosition;
+        self.readPosition += 1;
+    }
+};
---
+++ src/token.zig
@@ -0,0 +1,31 @@
+pub const TokenType = enum {
+    illegal,
+    eof,
+    ident,
+    int,
+    assign,
+    plus,
+    comma,
+    semicolon,
+    colon,
+    left_paren,
+    right_paren,
+    left_brace,
+    right_brace,
+
+    keyword_function,
+    keyword_let,
+    keyword_end,
+};
+
+pub const Token = struct {
+    type: TokenType,
+    literal: []const u8,
+
+    pub fn init(tokenType: TokenType, literal: []const u8) Token {
+        return Token{
+            .type = tokenType,
+            .literal = literal,
+        };
+    }
+};
